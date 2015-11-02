7f9655283f31403f82e3e7cc764dd2d32ba7f35a
jessicahuffstutler <jessica.huffstutler@gmail.com>
2015-11-02 14:23:03 -0500

    Insert and delete beer

--- src/com/theironyard/Main.java
+++ src/com/theironyard/Main.java
@@ -5,12 +5,39 @@ import spark.Session;
 import spark.Spark;
 import spark.template.mustache.MustacheTemplateEngine;
 
+import java.sql.*;
 import java.util.ArrayList;
 import java.util.HashMap;
 
 public class Main {
 
-    public static void main(String[] args) {
+    //Write a static method insertBeer
+    static void insertBeer(Connection conn, String name, String type) throws SQLException {
+        PreparedStatement stmt = conn.prepareStatement("INSERT INTO beers VALUES (?, ?)");
+        stmt.setString(1, name);
+        stmt.setString(2, type);
+        stmt.execute();
+    }
+
+    //Write a static method deleteBeer
+    static void deleteBeer (Connection conn, int idNum) throws SQLException {
+        PreparedStatement stmt = conn.prepareStatement("DELETE FROM beers WHERE ROWNUM = ?)");
+        stmt.setInt(1, idNum);
+        stmt.execute();
+    }
+
+    //Write a static method selectBeers that returns an ArrayList<Beer> containing all the beers in the database
+    static void selectBeers (Connection conn, String name, String type) {
+
+    }
+
+    public static void main(String[] args) throws SQLException {
+        //create the Connection
+        Connection conn = DriverManager.getConnection("jdbc:h2:./main");
+        Statement stmt = conn.createStatement();
+        //execute a query to create a beers table
+        stmt.execute("CREATE TABLE IF NOT EXISTS beers (name VARCHAR, type VARCHAR)");
+
         ArrayList<Beer> beers = new ArrayList();
         Spark.get(
                 "/",
@@ -44,18 +71,19 @@ public class Main {
                     beer.id = beers.size() + 1;
                     beer.name = request.queryParams("beername");
                     beer.type = request.queryParams("beertype");
+                    insertBeer(conn, beer.name, beer.type);
                     beers.add(beer);
                     response.redirect("/");
                     return "";
                 })
         );
-        Spark.post(
+        Spark.post( //doesn't delete beer when rownum integer is entered
                 "/delete-beer",
                 ((request, response) -> {
                     String id = request.queryParams("beerid");
                     try {
                         int idNum = Integer.valueOf(id);
-                        beers.remove(idNum-1);
+                        deleteBeer(conn, idNum);
                         for (int i = 0; i < beers.size(); i++) {
                             beers.get(i).id = i + 1;
                         }
